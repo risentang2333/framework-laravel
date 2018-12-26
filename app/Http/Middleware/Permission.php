c60d943821a72172f40cfb6b5361286a5095a768
TANGCHAO_D_WIN\tangchao_D_Win <tangchao@ihmair.cn>
2018-12-26 15:52:34 +0800

    修改中间件

---
+++ app/Http/Middleware/Permission.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Permission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        echo 1;exit;
+        return $next($request);
+    }
+}
